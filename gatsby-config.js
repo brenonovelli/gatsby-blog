1483d966ccb50acc1c11978e852f33214e36708d
Breno Novelli <bnojpa@gmail.com>
2020-04-12 19:38:30 -0300

    On going

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,22 @@
 module.exports = {
   siteMetadata: {
     title: `My Blog`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    position: `Creative Fullstack`,
+    description: `Pai na B&G MN. Em 2007 eu era web designer. Hoje não sei. Desenvolvedor frontend? Fullstack? UI? UX? Designer? Diretor de arte?`,
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
+    // needs to be the first to work with gatsby-remark-images
+    {
+      // keep as first gatsby-source-filesystem plugin for gatsby image support
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/static/assets/img`,
+        name: 'uploads',
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,8 +24,40 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/posts`,
+      },
+    },
     `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-relative-images',
+            options: {
+              name: 'uploads',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 960,
+              linkImagesToOriginal: false,
+            },
+          },
+          `gatsby-remark-lazy-load`,
+          `gatsby-remark-prismjs`,
+        ],
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -27,8 +70,9 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
---
+++ src/components/MenuBar/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import {
+  Bulb as Light,
+  SearchAlt2 as Search,
+  UpArrowAlt as Arrow,
+} from 'styled-icons/boxicons-regular/';
+
+import { Home, Grid } from 'styled-icons/boxicons-solid/';
+
+import * as S from './styled';
+
+const MenuBar = () => (
+  <S.MenuBarWrapper>
+    <S.MenuBarGroup>
+      <S.MenuBarLink to="/" title="Voltar para Home">
+        <S.MenuBarItem>
+          <Home />
+        </S.MenuBarItem>
+      </S.MenuBarLink>
+      <S.MenuBarLink to="/search/" title="Pesquisar">
+        <S.MenuBarItem>
+          <Search />
+        </S.MenuBarItem>
+      </S.MenuBarLink>
+    </S.MenuBarGroup>
+    <S.MenuBarGroup>
+      <S.MenuBarItem title="Mudar o tema">
+        <Light />
+      </S.MenuBarItem>
+      <S.MenuBarItem title="Mudar visualização">
+        <Grid />
+      </S.MenuBarItem>
+      <S.MenuBarItem title="Ir para o Topo">
+        <Arrow />
+      </S.MenuBarItem>
+    </S.MenuBarGroup>
+  </S.MenuBarWrapper>
+);
+
+export default MenuBar;
